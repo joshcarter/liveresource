47a10bef77306d053f8b40053b6e1b2e4c379a36
Josh Carter <joshc@spectralogic.com>
2010-07-23 17:52:37 +0000

    Compiler tests working.

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -5,65 +5,36 @@ require 'live_resource'
 
 resource_require_stub File.join(File.dirname(__FILE__), 'protos', 'compiler_test')
 
-module Protobuf
-  module Field
-    class FieldProxy
-      def typename_to_class(message_class, type)
-        names = type.to_s.split('::').map {|s| Util.camelize(s) }
-
-        puts "names: #{names}"
-
-        outer = message_class.to_s.split('::')
-
-        puts "outer: #{outer.join(' - ')}"
-
-        args = (Object.method(:const_defined?).arity == 1) ? [] : [false]
-
-        puts "args: #{args.join(' - ')}"
-
-        while
-            puts "will eval: #{outer.join('::')}"
-          mod = outer.empty? ? Object : eval(outer.join('::'))
-
-          puts "mod: #{mod}"
-
-          mod = names.inject(mod) {|m, s|
-            m && m.const_defined?(s, *args) && m.const_get(s)
-          }
-          break if mod
-          raise NameError.new("type not found: #{type}", type) if outer.empty?
-          outer.pop
-        end
-        mod
-      end
-    end
-  end
-end
-
 class CompilerTest < Test::Unit::TestCase
   def test_compile_generates_classes_and_modules
-    assert_equal Module, Compiler.class
-    assert_equal Module, Compiler::Test.class
-    assert_equal Class, Compiler::Test::ResponseHeader.class
-    assert_equal Class, Compiler::Test::ResponseHeader::MessageStatus.class
+    assert_equal Module, Test.class
+    assert_equal Class, Test::ResponseHeader.class
+    assert_equal Class, Test::ResponseHeader::MessageStatus.class
   end
 
   def test_compile_generates_enums
-    assert_equal Protobuf::EnumValue, Compiler::Test::ResponseHeader::MessageStatus::QUEUED.class
-    assert_equal Protobuf::EnumValue, Compiler::Test::Status::OK.class
+    assert_equal Protobuf::EnumValue, Test::ResponseHeader::MessageStatus::QUEUED.class
+    assert_equal Protobuf::EnumValue, Test::Status::OK.class
   end
 
   def test_message_assignment
-    fan = Compiler::Test::FanStatus.new
+    fan_status = Test::FanStatus.new
 
-    require 'pp'
-    pp fan.methods.sort
+    fan_status.status = Test::Status::OK
+    fan_status.rotations_per_minute = 1000
 
-    fan.status = Compiler::Test::Status::OK
-    fan.rotations_per_minute = 1000
+    assert_equal Test::Status::OK, fan_status.status
+    assert_equal 1000, fan_status.rotations_per_minute
 
-    assert_equal Compiler::Test::Status::OK, fan.status
-    assert_equal 1000, fan.rotations_per_minute
+    assert_raise(TypeError) { fan_status.status = "foo" }
+  end
+
+  def test_generates_stub_class
+    fan = Test::Fan::Stub.new
+
+    assert fan.methods.include?("get_fan_status")
+
+    assert_raise(NotImplementedError) { fan.get_fan_status }
   end
 end
 
