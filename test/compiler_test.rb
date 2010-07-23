0311f2b41af37d645978cb2fb992497ebf543cd6
Josh Carter <joshc@spectralogic.com>
2010-07-23 17:40:11 +0000

    Working on compiler tests, currently busted.

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -1,34 +1,69 @@
 require 'rubygems'
 require 'test/unit'
 require 'mocha'
-require 'resource'
+require 'live_resource'
+
+resource_require_stub File.join(File.dirname(__FILE__), 'protos', 'compiler_test')
+
+module Protobuf
+  module Field
+    class FieldProxy
+      def typename_to_class(message_class, type)
+        names = type.to_s.split('::').map {|s| Util.camelize(s) }
+
+        puts "names: #{names}"
+
+        outer = message_class.to_s.split('::')
+
+        puts "outer: #{outer.join(' - ')}"
+
+        args = (Object.method(:const_defined?).arity == 1) ? [] : [false]
+
+        puts "args: #{args.join(' - ')}"
+
+        while
+            puts "will eval: #{outer.join('::')}"
+          mod = outer.empty? ? Object : eval(outer.join('::'))
+
+          puts "mod: #{mod}"
+
+          mod = names.inject(mod) {|m, s|
+            m && m.const_defined?(s, *args) && m.const_get(s)
+          }
+          break if mod
+          raise NameError.new("type not found: #{type}", type) if outer.empty?
+          outer.pop
+        end
+        mod
+      end
+    end
+  end
+end
 
 class CompilerTest < Test::Unit::TestCase
-  def test_compile_generates_correct_classes_and_methods
-    progress_response = Resource::Response.new
-    progress_response.id = 0
-    progress_response.status = Resource::Response::Status::IN_PROGRESS
+  def test_compile_generates_classes_and_modules
+    assert_equal Module, Compiler.class
+    assert_equal Module, Compiler::Test.class
+    assert_equal Class, Compiler::Test::ResponseHeader.class
+    assert_equal Class, Compiler::Test::ResponseHeader::MessageStatus.class
+  end
 
-    done_response = Resource::Response.new
-    done_response.id = 0
-    done_response.status = Resource::Response::Status::OK
-    done_response.value = "alive"
+  def test_compile_generates_enums
+    assert_equal Protobuf::EnumValue, Compiler::Test::ResponseHeader::MessageStatus::QUEUED.class
+    assert_equal Protobuf::EnumValue, Compiler::Test::Status::OK.class
+  end
 
-    fan = Resource::Fan::Stub.new
+  def test_message_assignment
+    fan = Compiler::Test::FanStatus.new
 
-    # Fan should have status method already
-    assert(fan.methods.include? "status")
+    require 'pp'
+    pp fan.methods.sort
 
-    fan.expects(:status).times(3).returns(progress_response, progress_response, done_response)
+    fan.status = Compiler::Test::Status::OK
+    fan.rotations_per_minute = 1000
 
-    loop do
-      response = fan.status
-
-      next if (response.status == Resource::Response::Status::IN_PROGRESS)
-
-      assert_equal("alive", response.value)
-      break
-    end
+    assert_equal Compiler::Test::Status::OK, fan.status
+    assert_equal 1000, fan.rotations_per_minute
   end
 end
 
