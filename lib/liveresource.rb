d7fa87d7fe5fb0ec1fbc4d0ef461307d319cac8e
Josh Carter <public@joshcarter.com>
2011-04-10 13:36:34 -0600

    Removing everything for Redis-based do-over.

--- lib/liveresource.rb
+++
@@ -1,7 +0,0 @@
-require File::join(File::dirname(__FILE__), 'resource_compiler')
-require File::join(File::dirname(__FILE__), 'connection_tcp')
-require File::join(File::dirname(__FILE__), 'server_tcp')
-require File::join(File::dirname(__FILE__), 'client_tcp')
-require File::join(File::dirname(__FILE__), 'simple_message_codec')
-
-
