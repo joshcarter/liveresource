1d284bf29ef831e3b181acdd4910ce22561ce997
Mark von Minden <markv@spectralogic.com>
2012-09-04 16:48:43 -0600

    Fixed validate_method routine to properly check methods with splat params. Add method parameters tests.

---
+++ test/method_params_test.rb
@@ -0,0 +1,79 @@
+require_relative 'test_helper'
+
+class ParamsTest < Test::Unit::TestCase
+  class ParamsClass
+    include LiveResource::Resource
+
+    remote_reader :name
+
+    resource_name :name
+    resource_class :params_class
+
+    def initialize(name)
+      remote_attribute_write(:name, name)
+    end
+
+    def no_params_method
+      true
+    end
+
+    def fixed_params_method(arg1, arg2)
+      arg1 + arg2
+    end
+
+    def splat_params_method(*params)
+      params.length
+    end
+
+    def mixed_params_method(arg1, arg2, *params)
+      result = arg1 + arg2
+      params.each do |param|
+        result = result + param
+      end
+      result
+    end
+  end
+
+  def setup
+    Redis.new.flushall
+
+    LiveResource::RedisClient.logger.level = Logger::INFO
+
+    # Class resources
+    LiveResource::register ParamsClass
+
+    @test_class = LiveResource::find(:params_class)
+    @test_instance = @test_class.new("bob")
+  end
+
+  def teardown
+    LiveResource::stop
+  end
+
+  def test_no_params_method
+    assert_equal true, @test_instance.no_params_method
+    assert_raise(ArgumentError) { @test_instance.no_params_method("foo") }
+  end
+
+  def test_fixed_params_method
+    assert_equal 13, @test_instance.fixed_params_method(6, 7)
+    assert_raise(ArgumentError) { @test_instance.fixed_params_method }
+    assert_raise(ArgumentError) { @test_instance.fixed_params_method(6, 7, 8) }
+  end
+
+  def test_splat_params
+    assert_equal 0, @test_instance.splat_params_method
+    assert_equal 1, @test_instance.splat_params_method("foo")
+    assert_equal 2, @test_instance.splat_params_method("foo", "bar")
+    assert_equal 3, @test_instance.splat_params_method("foo", "bar", "baz")
+  end
+
+  def test_mixed_params_method
+    assert_equal 3, @test_instance.mixed_params_method(1, 2)
+    assert_equal 6, @test_instance.mixed_params_method(1, 2, 3)
+    assert_equal 10, @test_instance.mixed_params_method(1, 2, 3, 4)
+
+    assert_raise(ArgumentError) { @test_instance.mixed_params_method }
+    assert_raise(ArgumentError) { @test_instance.mixed_params_method(1) }
+  end
+end
