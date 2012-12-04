49d4dd862d17513beee3547486edede8f4b263c9
Mark von Minden <markv@spectralogic.com>
2012-12-04 14:19:33 -0700

    Detect potentially confusing case where the resource_name is generating by reading a remote_attribute. Since reading a remote_attribute requires the resource_name, this creates a circular dependency that ultimately leads to a SystemStackError.
    We now catch this case and raise an exception with a more useful
    error message.

---
+++ test/resource_name_test.rb
@@ -0,0 +1,68 @@
+require_relative 'test_helper'
+
+class ResourceNameTest < Test::Unit::TestCase
+  class Class1
+    include LiveResource::Resource
+
+    resource_class :class_1
+    resource_name :name
+
+    remote_reader :name
+
+    def initialize(name)
+      remote_attribute_write(:name, name)
+    end
+  end
+
+  class Class2
+    include LiveResource::Resource
+
+    resource_class :class_2
+    resource_name :name_method
+
+    remote_reader :name
+
+    def initialize(name)
+      remote_attribute_write(:name, name)
+    end
+
+    def name_method
+      # This won't work!
+      #
+      # In order to get the name attribute, we have to already know the
+      # name of the resource (but this method is trying to get the name
+      # of the resource). Chicken. Egg.
+      self.name
+    end
+  end
+
+  def setup
+    Redis.new.flushall
+
+    LiveResource::RedisClient.logger.level = Logger::INFO
+
+    LiveResource::register Class1
+    LiveResource::register Class2
+  end
+
+  def teardown
+    LiveResource::stop
+  end
+
+  def test_resource_name_based_on_remote_attr
+    c1 = LiveResource::find(:class_1)
+    c1_instance = c1.new("foo")
+
+    # Make sure we can find it by the name
+    assert_not_nil LiveResource::find(:class_1, "foo")
+
+    # Check the name
+    assert_equal "foo", c1_instance.name
+  end
+
+  def test_resource_name_circular_dependency
+    assert_raise RuntimeError do
+      LiveResource::find(:class_2).new("foo")
+    end
+  end
+end
